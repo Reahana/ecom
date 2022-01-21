8923916f69c3317225e0ad1d2927af8e772e3475
Amanda Reahana Gomes <reahana233@gmail.com>
2022-01-21 09:13:13 +0600

    brand

---
+++ resources/views/admin/brand/manage.blade.php
@@ -0,0 +1,61 @@
+@extends('admin.master')
+
+@section('title')
+    Manage Brand
+@endsection
+
+@section('body')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="ibox">
+                <div class="ibox-head">
+                    <div class="ibox-title">All brand info goes here</div>
+                </div>
+                <div class="ibox-body">
+                    @if($message = Session::get('message'))
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>{{$message}}!</strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    <table class="table table-striped table-bordered table-hover" id="example-table" cellspacing="0" width="100%">
+                        <thead>
+                        <tr>
+                            <th>SL No</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($brands as $brand)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$brand->name}}</td>
+                                <td>{{$brand->description}}</td>
+                                <td><img src="{{asset($brand->image)}}" alt="" height="70" width="100"></td>
+                                <td>{{$brand->status == 1 ? 'Published' : 'Unpublished'}}</td>
+                                <td>
+                                    <a href="{{route('brand.edit', ['id' => $brand->id])}}" class="btn btn-success btn-xs">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                    <a href="" class="btn btn-danger btn-xs" onclick="event.preventDefault(); document.getElementById('brandDeleteForm{{$brand->id}}').submit();">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                    <form action="{{route('brand.delete', ['id' => $brand->id])}}" method="POST" id="brandDeleteForm{{$brand->id}}">
+                                        @csrf
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
