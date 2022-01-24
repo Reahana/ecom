0aaab7e74c180f3f1e153dab13bc24af43c9a470
Amanda Reahana Gomes <reahana233@gmail.com>
2022-01-24 11:39:31 +0600

    login

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width initial-scale=1.0"/>
-    <title>Admincast bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Login</title>
+    <title>Admin | Login</title>
     <!-- GLOBAL MAINLY STYLES-->
     <link href="{{asset('/admin')}}/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="{{asset('/admin')}}/assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
