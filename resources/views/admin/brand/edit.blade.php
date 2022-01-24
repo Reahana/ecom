1445ba1f78054cb81a535af1d95dc2bfbba7dd63
Amanda Reahana Gomes <reahana233@gmail.com>
2022-01-24 09:40:08 +0600

    brand edit

--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -1,7 +1,65 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Web App Develop-PHP
- * Date: 12/29/2021
- * Time: 3:27 PM
- */
+@extends('admin.master')
+
+@section('title')
+   Edit Brand
+@endsection
+
+@section('body')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="ibox">
+                <div class="ibox-head">
+                    <div class="ibox-title">Edit Brand Form</div>
+                    <div class="ibox-tools">
+                        <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
+                        <a class="fullscreen-link"><i class="fa fa-expand"></i></a>
+                    </div>
+                </div>
+                <div class="ibox-body">
+                    @if($message = Session::get('message'))
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>{{$message}}!</strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    <form class="form-horizontal" action="{{route('brand.update', ['id' => $brand->id])}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Brand Name</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="text" value="{{$brand->name}}" name="name" placeholder="Brand Name"/>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Brand Description</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" name="description" value="{{$brand->description}} placeholder="Brand Description"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Brand Image</label>
+                            <div class="col-sm-10">
+                                <input class="form-control-file" type="file" name="image" accept="image/*"/>
+                                <img src="{{asset($brand->image)}}" alt="" height="50" width="80"/>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Publication Status</label>
+                            <div class="col-sm-10">
+                                <label><input type="radio" name="status" {{$brand->status == 1 ? 'checked' : '' }} value="1"/> Published</label>
+                                <label><input type="radio" name="status" {{$brand->status == 0 ? 'checked' : '' }} value="0"/> UnPublished</label>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-10 ml-sm-auto">
+                                <button class="btn btn-info btn-block" type="submit">Edit Brand</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
