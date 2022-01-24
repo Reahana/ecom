2f61bdef0b5f558ebbaf9ad034887cc573cdfd7a
Amanda Reahana Gomes <reahana233@gmail.com>
2022-01-24 11:40:23 +0600

    unit edit

--- resources/views/admin/unit/edit.blade.php
+++ resources/views/admin/unit/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.master')
 
 @section('title')
-    Add New Unit
+   Edit Unit
 @endsection
 
 @section('body')
@@ -9,7 +9,7 @@
         <div class="col-md-12">
             <div class="ibox">
                 <div class="ibox-head">
-                    <div class="ibox-title">Add Unit Form</div>
+                    <div class="ibox-title">Edit Unit Form</div>
                     <div class="ibox-tools">
                         <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
                         <a class="fullscreen-link"><i class="fa fa-expand"></i></a>
@@ -24,30 +24,30 @@
                             </button>
                         </div>
                     @endif
-                    <form class="form-horizontal" action="{{route('unit.new')}}" method="POST">
+                    <form class="form-horizontal" action="{{route('unit.new', ['id' => $unit->id])}}" method="POST">
                         @csrf
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">Unit Name</label>
                             <div class="col-sm-10">
-                                <input class="form-control" type="text" name="name" placeholder="Unit Name"/>
+                                <input class="form-control" type="text"  value="{{$unit->name}} name="name" placeholder="Unit Name"/>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">Unit Description</label>
                             <div class="col-sm-10">
-                                <textarea class="form-control" name="description" placeholder="Unit Description"></textarea>
+                                <textarea class="form-control" name="description" value="{{$unit->description}} placeholder="Unit Description"></textarea>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">Publication Status</label>
                             <div class="col-sm-10">
-                                <label><input type="radio" name="status" checked value="1"/> Published</label>
-                                <label><input type="radio" name="status" value="0"/> UnPublished</label>
+                                <label><input type="radio" {{$unit->status == 1 ? 'checked' : '' }}  name="status"  value="1"/> Published</label>
+                                <label><input type="radio" {{$unit->status == 0 ? 'checked' : '' }} name="status" value="0"/> UnPublished</label>
                             </div>
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-10 ml-sm-auto">
-                                <button class="btn btn-info btn-block" type="submit">Create New Unit</button>
+                                <button class="btn btn-success btn-block" type="submit"> Update Unit</button>
                             </div>
                         </div>
                     </form>
