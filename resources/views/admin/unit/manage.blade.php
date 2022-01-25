04006cc8e110d0243c63fdc7742173c923db0320
Amanda Reahana Gomes <reahana233@gmail.com>
2022-01-25 10:19:21 +0600

    unit manage

--- resources/views/admin/unit/manage.blade.php
+++ resources/views/admin/unit/manage.blade.php
@@ -13,7 +13,7 @@
                 </div>
                 <div class="ibox-body">
                     @if($message = Session::get('message'))
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
                             <strong>{{$message}}!</strong>
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
