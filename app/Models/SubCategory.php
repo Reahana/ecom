8892680c80d8c87e4c87da1594c7ff8cd50906c5
Amanda Reahana Gomes <reahana233@gmail.com>
2022-05-01 11:39:59 +0600

    updated

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -88,7 +88,8 @@ public function category ()
         return $this->belongsTo('App\Models\Category');
     }
 
-    public function products ()
+  
+  public function products ()
     {
         return $this->hasMany('App\Models\Product');
     }
